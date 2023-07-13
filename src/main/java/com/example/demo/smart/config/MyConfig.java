a1c4a729f0d1d9b053d31fbd13d3f4f2d4cae8f4
Deep5656 <deepanshukatiyar5656@gmail.com>
2023-07-13 08:07:20 +0530

    initial commit

---
+++ src/main/java/com/example/demo/smart/config/MyConfig.java
@@ -0,0 +1,8 @@
+package com.example.demo.smart.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+    
+}
---
+++ src/main/java/com/example/demo/smart/controller/HomeController.java
@@ -0,0 +1,84 @@
+package com.example.demo.smart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.smart.dao.UserRepository;
+import com.example.demo.smart.entities.User;
+import com.example.demo.smart.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@RequestMapping("/")
+	public String home(ModelMap model) {
+		model.addAttribute("title", "Home - Smart Contact Manager.");
+		return "home";
+	}
+
+	@RequestMapping("/about")
+	public String about(ModelMap model) {
+		model.addAttribute("title", "About - Smart Contact Manager.");
+		return "about";
+	}
+
+	@RequestMapping("/signup")
+	public String signup(ModelMap model) {
+		model.addAttribute("title", "Register - Smart Contact Manager");
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+
+	@RequestMapping(value = "/do_register", method = RequestMethod.POST)
+	public String registerUser(@Valid @ModelAttribute("user") User user,BindingResult result1,
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, ModelMap model,
+			HttpSession session) {
+		try {
+			if (!agreement) {
+				System.out.println("You have not agreed the terms & condition");
+				throw new Exception("You have not agreed the terms & condition");
+			}
+
+			if(result1.hasErrors()){
+				System.out.println("Error "+result1.toString());
+				model.addAttribute("user", user);
+				return "signup";
+			}
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImageurl("default.png");
+			System.out.println("agreement " + agreement);
+			System.out.println("USER" + user);
+			User result = this.userRepository.save(user);
+			model.addAttribute("user", new User());
+			// session.setAttribute("Successfully Registered !!", "alert-success");
+			session.setAttribute("message", new Message("Successfully Registered !!", "alert-success"));
+			return "signup";
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("Something went wrong !!" + e.getMessage(), "alert-danger"));
+			return "signup";
+
+		}
+	}
+
+	@RequestMapping("/login")
+	public String login(ModelMap model) {
+		model.addAttribute("title", "Register - Smart Contact Manager.");
+		return "login";
+	}
+}
